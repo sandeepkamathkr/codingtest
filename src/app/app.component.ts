03917a73a3b7a380c1a30b3b7539711f874350aa
Sandeep <sandeepkamathkr@gmail.com>
2020-03-17 01:04:38 +1100

    getting users and displaying

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {UsersService} from "./config/users.service";
+import {User} from "./shared/model/user.model";
+import {Subscription} from "rxjs";
+import {finalize} from "rxjs/operators";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'codingtest';
+export class AppComponent implements OnInit {
+  isLoading = false;
+  users: User[];
+  private sub$: Subscription = new Subscription();
+
+  constructor(private usersService: UsersService) {
+  }
+
+  ngOnInit(): void {
+    this.isLoading = true;
+    this.sub$.add(this.usersService.findAllUsers()
+      .pipe(finalize(() => (this.isLoading = false)))
+      .subscribe(result => {
+        this.users = result;
+        this.users.forEach(user => {
+          const name:string[] =  user.name.split(" ");
+          if(name.length ==3){
+            user.name = name[1];
+          }else{
+            user.name = name[0];
+          }
+        });
+      }));
+  }
 }
